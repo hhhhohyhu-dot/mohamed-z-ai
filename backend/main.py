3e98e7d87da1160fdd27813240398ec87e759f3f
mohamed <hhhhohyhu@gmail.com>
2026-07-14 00:24:42 +0100

    test

--- backend/main.py
+++ backend/main.py
@@ -1,28 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.api.dashboard import router as dashboard_router
-from app.api.market import router as market_router
-from app.api.news import router as news_router
-
 
 app = FastAPI(
     title="Mohamed Z AI API",
-    version="1.0.0"
+    version="1.0.0",
 )
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-# Routers
-app.include_router(market_router)
-app.include_router(news_router)
-app.include_router(dashboard_router)
-
 
 @app.get("/")
 async def home():
@@ -30,7 +20,6 @@ async def home():
         "success": True,
         "message": "Mohamed Z AI Backend is Running 🚀"
     }
-
 
 @app.get("/health")
 async def health():
