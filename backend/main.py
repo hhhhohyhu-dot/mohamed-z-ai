08f6e54ed02c4d031cf838de40dcbc727e2711b8
mohamed <hhhhohyhu@gmail.com>
2026-07-14 13:18:38 +0100

    Test simple app

--- backend/main.py
+++ backend/main.py
@@ -4,12 +4,12 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {
-        "message": "OK"
-    }
+    return {"message": "OK"}
 
 @app.get("/health")
 async def health():
-    return {
-        "status": "healthy"
-    }
+    return {"status": "healthy"}
+
+@app.get("/docs-test")
+async def docs_test():
+    return {"working": True}
